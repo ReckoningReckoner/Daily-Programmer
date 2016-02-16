fb70e7569a945db6fbf4cb48fadce0359ed790df
Viraj Bangari <Viraj3f@gmail.com>
2016-02-16 02:16:48 -0500

    converted tabs to spaces

--- 253h/Input.java
+++ 253h/Input.java
@@ -1,35 +1,35 @@
 class Input
 {
-	public static void main(String args[])
-	{
-		Terminal t = new Terminal();
-		String input1 = "^h^c^iDDD^r^rPPPP^d^bD^r^rD^rP^19P^d^bD^r^rD^rPPPP^d^bD^r^rD^rP^d^bDDD^r^rP";
-		String input2 = "^h^c^i^04^^^13/ Y^d^b  /   Y^u^d^d^l^l^l^l^l^l^l^l^l^r^r^l^l^d<^48>^l^l^d/^b^o Y^d^r^r^66/^b  Y^b^d   Y /^d^l^lv^d^b===========^i^94O123456789^94A=======^u^u^u^u^u^u^l^lY^o^b^r/";
-		run(t, input1);
-		run(t, input2);
-	}
+    public static void main(String args[])
+    {
+        Terminal t = new Terminal();
+        String input1 = "^h^c^iDDD^r^rPPPP^d^bD^r^rD^rP^19P^d^bD^r^rD^rPPPP^d^bD^r^rD^rP^d^bDDD^r^rP";
+        String input2 = "^h^c^i^04^^^13/ Y^d^b  /   Y^u^d^d^l^l^l^l^l^l^l^l^l^r^r^l^l^d<^48>^l^l^d/^b^o Y^d^r^r^66/^b  Y^b^d   Y /^d^l^lv^d^b===========^i^94O123456789^94A=======^u^u^u^u^u^u^l^lY^o^b^r/";
+        run(t, input1);
+        run(t, input2);
+    }
 
 
-	public static void run(Terminal t, String input1)
-	{
-		int i = 0;
-		while (i < input1.length()){
-			if (input1.substring(i, i+1).equals("^")) {
-				if (Character.isDigit(input1.charAt(i+1))) {
-					t.parse(input1.substring(i, i+3));
-					i += 3;
-				}
-			   	else {
-					t.parse(input1.substring(i, i+2));
-					i += 2;
-				}
-			}
-			else {
-				t.parse(input1.substring(i, i+1));
-				i ++;
-			}
-		}
+    public static void run(Terminal t, String input1)
+    {
+        int i = 0;
+        while (i < input1.length()){
+            if (input1.substring(i, i+1).equals("^")) {
+                if (Character.isDigit(input1.charAt(i+1))) {
+                    t.parse(input1.substring(i, i+3));
+                    i += 3;
+                }
+               else {
+                    t.parse(input1.substring(i, i+2));
+                    i += 2;
+                }
+            }
+            else {
+                t.parse(input1.substring(i, i+1));
+                i ++;
+            }
+        }
 
-		t.display();
-	}
+        t.display();
+    }
 }
--- 253h/Terminal.java
+++ 253h/Terminal.java
@@ -1,139 +1,139 @@
 class Terminal
 {
-	private String screen[][];
-	private int x_cursor;
-	private int y_cursor;
-	private int height;
-	private int width;
-	private boolean insert_mode = true;
+    private String screen[][];
+    private int x_cursor;
+    private int y_cursor;
+    private int height;
+    private int width;
+    private boolean insert_mode = true;
 
-	public Terminal(int h, int w, int x_cursor, int y_cursor)
-	{
-		this.screen = new String[h][w];
-		this.height = h;
-		this.width = w;
-		this.x_cursor = x_cursor;
-		this.y_cursor = y_cursor;
-	}
+    public Terminal(int h, int w, int x_cursor, int y_cursor)
+    {
+        this.screen = new String[h][w];
+        this.height = h;
+        this.width = w;
+        this.x_cursor = x_cursor;
+        this.y_cursor = y_cursor;
+    }
 
-	public Terminal()
-	{
-		this(10, 10, 0, 0);
-	}
+    public Terminal()
+    {
+        this(10, 10, 0, 0);
+    }
 
-	public int range(int val, int max)
-	{
-		if (val < 0){
-			return 0;
-		}
-		else if (val > max){
-			return max;
-		}
-		else {
-			return val;
-		}
-	}
+    public int range(int val, int max)
+    {
+        if (val < 0){
+            return 0;
+        }
+        else if (val > max){
+            return max;
+        }
+        else {
+            return val;
+        }
+    }
 
-	private void set_cursor(int x, int y)
-	{
-		x_cursor = range(x, width-1);
-		y_cursor = range(y, height-1);
-	}
-	
-	private void clean()
-	{
-		for (int y = 0; y < height; y++){
-			for (int x = 0; x < width; x++){
-				screen[y][x] = null;
-			}
-		}
-	}
+    private void set_cursor(int x, int y)
+    {
+        x_cursor = range(x, width-1);
+        y_cursor = range(y, height-1);
+    }
+    
+    private void clean()
+    {
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                screen[y][x] = null;
+            }
+        }
+    }
 
-	private void write(String val)
-	{
-		screen[y_cursor][x_cursor] = val;
-	}
+    private void write(String val)
+    {
+        screen[y_cursor][x_cursor] = val;
+    }
 
-	private void erase_row()
-	{
-		for (int i = x_cursor; i < width; i++)
-			screen[y_cursor][i] = null;
-	}
+    private void erase_row()
+    {
+        for (int i = x_cursor; i < width; i++)
+            screen[y_cursor][i] = null;
+    }
 
-	private void shift_forward()
-	{
-		for (int x = width-1; x > x_cursor; x--) {
-			screen[y_cursor][x] = screen[y_cursor][x-1];
-		}
-	}
-	
-	private void insert(String val)
-	{
-		if (insert_mode) shift_forward();
-		write(val);
-		set_cursor(x_cursor+1, y_cursor);
-	}
+    private void shift_forward()
+    {
+        for (int x = width-1; x > x_cursor; x--) {
+            screen[y_cursor][x] = screen[y_cursor][x-1];
+        }
+    }
+    
+    private void insert(String val)
+    {
+        if (insert_mode) shift_forward();
+        write(val);
+        set_cursor(x_cursor+1, y_cursor);
+    }
 
-	public void parse(String input)
-	{
-		if (input.substring(0, 1).equals("^")) {
-			switch (input){
-				case "^c":
-					clean();
-					break;
-				case "^h":
-					set_cursor(0, 0);
-					break;
-				case "^b":
-					set_cursor(0, y_cursor);
-					break;
-				case "^d":
-					set_cursor(x_cursor, y_cursor + 1);
-					break;
-				case "^u":
-					set_cursor(x_cursor, y_cursor - 1);
-					break;
-				case "^l":
-					set_cursor(x_cursor - 1, y_cursor);
-					break;
-				case "^r":
-					set_cursor(x_cursor + 1, y_cursor);
-					break;
-				case "^e": 
-					erase_row();
-					break;
-				case "^i":
-					insert_mode = true;
-					break;
-				case "^o":
-					insert_mode = false;
-					break;
-				case "^^":
-					insert("^");
-					break;
-				default:
-					set_cursor(Integer.parseInt(input.substring(2,3)),
-							Integer.parseInt(input.substring(1, 2)));
-					break;
-			}
-		} 
-		else {
-			insert(input);
-		}
-	}
-	
-	public void display()
-	{
-		for (int y = 0; y < height; y ++){
-			for (int x = 0; x < width; x++){
-				if (screen[y][x] != null) {
-					System.out.print(screen[y][x]);
-				}
-				else {
-					System.out.print(" ");
-				}
-			}
-			System.out.print("\n");	
-		}
-	}
+    public void parse(String input)
+    {
+        if (input.substring(0, 1).equals("^")) {
+            switch (input){
+                case "^c":
+                    clean();
+                    break;
+                case "^h":
+                    set_cursor(0, 0);
+                    break;
+                case "^b":
+                    set_cursor(0, y_cursor);
+                    break;
+                case "^d":
+                    set_cursor(x_cursor, y_cursor + 1);
+                    break;
+                case "^u":
+                    set_cursor(x_cursor, y_cursor - 1);
+                    break;
+                case "^l":
+                    set_cursor(x_cursor - 1, y_cursor);
+                    break;
+                case "^r":
+                    set_cursor(x_cursor + 1, y_cursor);
+                    break;
+                case "^e": 
+                    erase_row();
+                    break;
+                case "^i":
+                    insert_mode = true;
+                    break;
+                case "^o":
+                    insert_mode = false;
+                    break;
+                case "^^":
+                    insert("^");
+                    break;
+                default:
+                    set_cursor(Integer.parseInt(input.substring(2,3)),
+                            Integer.parseInt(input.substring(1, 2)));
+                    break;
+            }
+        } 
+        else {
+            insert(input);
+        }
+    }
+    
+    public void display()
+    {
+        for (int y = 0; y < height; y ++){
+            for (int x = 0; x < width; x++){
+                if (screen[y][x] != null) {
+                    System.out.print(screen[y][x]);
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");    
+        }
+    }
 }
